1a36ef034703e0e4fca14c8b2eed216955060936
Arun Tito <thearuntito@gmail.com>
2026-06-01 22:01:06 +0530

    add debug route

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\RedirectController;
-
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/debug', function () {
+    return response()->json([
+        'smxm' => config('profiles.smxm'),
+        'routes_loaded' => true,
+    ]);
+});
 
 Route::get('/r/{link}', [RedirectController::class, 'redirect'])->name('links.redirect');
 
