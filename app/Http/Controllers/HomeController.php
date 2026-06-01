ae4f35044d9c95219f2f4204e1bc9840d8d0ec6a
Arun Dharavath <thearuntito@gmail.com>
2026-06-01 20:59:13 +0530

    Add index method to HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Profile;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    
+    public function index()
+    {
+        return view('home.index', [
+            'profiles' => collect(),
+        ]);
+    }
+}
