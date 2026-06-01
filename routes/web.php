ca7e8f74d12c84c61dd26a59b542673040c5b00f
Arun Tito <thearuntito@gmail.com>
2026-06-01 22:13:26 +0530

    Fix missing PHP tag and imports in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RedirectController;
+
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/debug', function () {
